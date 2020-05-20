0f361f6d83d514cc271952243ae499af7813d7f5
pklimesh <63873212+pklimesh@users.noreply.github.com>
2020-05-20 18:56:00 -0500

    WishList 3-4

---
+++ WishList/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Data
+{
+    public class DbContext
+    {
+        public readonly IGetDataRepository _data;
+    }
+    {
+        AddDbContext<ApplicationDbContext>
+    }
+}
---
+++ WishList/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        PublicId Int;
+    }
+}
